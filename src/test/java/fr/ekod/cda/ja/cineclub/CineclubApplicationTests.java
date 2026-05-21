0535366e1d9cc24643f24d798eebe4d5cf1cbb9c
Rexil-Dev <fuseaunoir@gmail.com>
2026-05-21 10:22:15 +0200

    Suppression des tests

--- src/test/java/fr/ekod/cda/ja/cineclub/CineclubApplicationTests.java
+++ src/test/java/fr/ekod/cda/ja/cineclub/CineclubApplicationTests.java
@@ -1,12 +1,6 @@
 package fr.ekod.cda.ja.cineclub;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CineclubApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 }
